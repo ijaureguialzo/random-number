4eceac0efacfab9e857df5030ed15c49ca1946e3
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-02-15 12:37:35 +0100

    Generador de valores al azar

--- RandomNumber/RandomNumber/ContentView.swift
+++ RandomNumber/RandomNumber/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var numero = 0
+    @State private var digitos = 2
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        VStack {
+
+            Text(String(numero))
+                .padding(30)
+                .frame(width: 300, height: 300, alignment: .center)
+                .background(Color.black)
+                .clipShape(Circle())
+                .foregroundColor(Color.white)
+                .font(.system(size: 200))
+                .minimumScaleFactor(0.0001)
+                .lineLimit(1)
+                .padding()
+                .onChange(of: digitos) { _ in
+                aleatorio()
+            }
+
+            Stepper("Dígitos", value: $digitos, in: 1...10)
+                .labelsHidden()
+                .padding()
+
+            Button(action: {
+                aleatorio()
+            }) {
+                Image(systemName: "forward.fill")
+                    .font(.title)
+            }.padding()
+
+        }.onAppear() {
+            aleatorio()
+        }
+    }
+
+    func aleatorio() {
+        numero = Int.random(in: Int(pow(Double(10), Double(digitos - 1)))..<Int(pow(Double(10), Double(digitos))))
     }
 }
 
