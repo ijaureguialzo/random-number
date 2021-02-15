d754a3f4b2e4384207d69e1f04e5389691968145
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-02-15 11:48:24 +0100

    Nuevo proyecto

---
+++ RandomNumber/RandomNumber/RandomNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberApp.swift
+//  RandomNumber
+//
+//  Created by Ion Jaureguialzo Sarasola on 15/2/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
